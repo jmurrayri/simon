6d7aafbb0ada6f3a424cfd764387be7d02dd133e
Jason Murray <jmurrayri@gmail.com>
2019-04-10 20:53:34 -0400

    Updates

---
+++ precache-manifest.4ac19583bfeaba494fa740819d09b9d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd63f190e946a41f7d5b",
+    "url": "/simon/static/js/runtime~main.24bf3572.js"
+  },
+  {
+    "revision": "d0705d1461676f3a2299",
+    "url": "/simon/static/js/main.d37eb361.chunk.js"
+  },
+  {
+    "revision": "56e62f93fb8782db1feb",
+    "url": "/simon/static/js/2.d674bac7.chunk.js"
+  },
+  {
+    "revision": "d0705d1461676f3a2299",
+    "url": "/simon/static/css/main.43cfc624.chunk.css"
+  },
+  {
+    "revision": "f5b7656e5727f33f54c32c0bb3153206",
+    "url": "/simon/index.html"
+  }
+];
